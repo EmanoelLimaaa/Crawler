88247b8b16ae943bb3479d6cc91e425671fe9efb
Ramon-Leandro <128464568+Ramon-Leandro@users.noreply.github.com>
2026-05-03 14:00:39 -0300

    Instalando .venv e criando o serviço aonde vai pegar o dados e organiza-la

--- api/app/rotas/busca.py
+++ api/app/rotas/busca.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 from app.servicos.servico_busca import buscar_no_indice
 
-# Importamos o APIRouter para organizar as rotas em arquivos separados
-# Importando a função de lógica que foi criano no arquivo de serviço
+# Importado o APIRouter para organizar as rotas em arquivos separados
+# Importado a função de lógica que foi criano no arquivo de serviço
 
-# Criamos o objeto 'router' que será registrado no arquivo principal (main.py)
+# Criado o objeto 'router' que será registrado no arquivo principal (main.py)
 router = APIRouter()
 
-# Definimos que, quando alguém acessar "/pesquisar" via método GET:
+# Definido que, quando alguém acessar "/pesquisar" via método GET:
 @router.get("/pesquisar")
 def pesquisar(q: str):
     resultados = buscar_no_indice(q)
