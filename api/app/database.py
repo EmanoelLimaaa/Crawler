e107024932e0762cea52a62dd8a0f66a656f77b8
Ramon-Leandro <128464568+Ramon-Leandro@users.noreply.github.com>
2026-05-03 16:46:21 -0300

    Funcionamento

--- api/app/database.py
+++ api/app/database.py
@@ -1,30 +1,12 @@
 import os
+import json
 
-# Definindo o nome do arquivo que o Rust vai criar
-ARQUIVO_INDICE = "indice.txt"
+# Caminho exato onde o Rust salvou o arquivo[cite: 1]
+ARQUIVO_INDICE = "../compartilhado/saida/index.json"
 
 def ler_dados_do_rust():
-    
-    #Esta função abre o arquivo gerado pelo motor em Rust, lê cada linha e transforma em uma lista de dicionários que o Python consegue entender.
-    
-    lista_arquivos = []
-
-    # Proteção: Se o arquivo não existir, a API não trava
     if not os.path.exists(ARQUIVO_INDICE):
-        return [{"arquivo": "Aviso", "local": "Aguardando motor Rust...", "tags": ""}]
+        return {} # Retorna dicionário vazio se não achar o arquivo
 
-    # Leitura eficiente linha por linha
-    with open(ARQUIVO_INDICE, "r", encoding="utf-8") as arquivo:
-        for linha in arquivo:
-            caminho_completo = linha.strip()
-            
-            if caminho_completo:
-                # Transforma o caminho bruto em um objeto estruturado
-                item = {
-                    "arquivo": os.path.basename(caminho_completo),
-                    "local": caminho_completo,
-                    "tags": "rust_engine"
-                }
-                lista_arquivos.append(item)
-                
-    return lista_arquivos
+    with open(ARQUIVO_INDICE, "r", encoding="utf-8") as f:
+        return json.load(f) # Transforma o JSON em um dicionário do Python
